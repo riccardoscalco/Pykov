174fd46be89e39be73d7084bc985d85a21eb7b85
Riccardo Scalco <riccardo.scalco@gmail.com>
2012-02-06 16:57:53 +0100

    On branch master
    	new file:   MANIFEST
    	new file:   dist/pykov-1.0.tar.gz
    	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='pykov',
+      version='1.0',
+      description='A Markov Chain Python module',
+      author='Riccardo Scalco',
+      author_email='riccardo.scalco@gmail.com',
+      url='https://github.com/riccardoscalco/Pykov',
+      py_modules=['pykov'],
+      )
+
